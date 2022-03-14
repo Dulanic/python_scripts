9b006922b37d60f14e8218f104d859a426483ee8
Dulanic <dulanic@gmail.com>
2022-03-14 16:13:16 +0000

    Last cleanup

--- weather/from noaa_sdk import NOAA.py
+++
@@ -1,6 +0,0 @@
-from zoneinfo import ZoneInfo
-from datetime import datetime, timedelta, date, timezone
-CHICAGO = ZoneInfo("America/Chicago")
-
-today = datetime.today().astimezone(CHICAGO)
-print(today)
