5bb2e5c45ed625e26f139f46fc0dbdf78a4dec23
Dulanic <dulanic@gmail.com>
2022-03-14 16:07:25 +0000

    Cleanup

--- test/plex.py
+++
@@ -1,5 +0,0 @@
-from plexapi.server import PlexServer
-
-
-plex = PlexServer('https://plex.dulanic.com', 'wyuHqnx68uoERVdNayMz')
-print(1)
--- test/rock.py
+++
@@ -1,5 +0,0 @@
-import os
-a = ['testing','testing1']
-tuples = [tuple(x) for x in df.to_numpy()]
-tuples = tuple( =tuple(a)
-print(a)
